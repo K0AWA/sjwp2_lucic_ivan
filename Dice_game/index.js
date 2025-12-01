81810a10f55bbe956e3f48dc58a5633cc2318293
lucic <ivanlucic946@gmail.com>
2025-12-01 17:27:41 +0100

    k

--- Dice_game/index.js
+++ Dice_game/index.js
@@ -21,3 +21,54 @@ else if(randNumber2>randNumber1) {
 else{
     alert("It is draw")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*1. Kako pristupiti h1 elementu preko njegovog ID-ja title i promijeniti mu boju u crvenu?
+document.getElementById("title").style.color = "red";
+
+2. Kako pomoću getElementsByTagName odabrati sve <li> elemente i prvome promijeniti tekst u “Promijenjeno”?
+document.getElementsByTagName("li")[0].textContent = "Promijenjeno";
+
+3. Kako dohvatiti sve elemente s klasom list pomoću getElementsByClassName i zadnjem dodati klasu highlight?
+let items = document.getElementsByClassName("list");
+items[items.length - 1].classList.add("highlight");
+
+4. Kako pomoću querySelector odabrati prvi <a> element unutar liste i promijeniti mu atribut href na https://www.bing.com
+?
+document.querySelector("ul a").setAttribute("href", "https://www.bing.com");
+
+5. Kako pomoću querySelectorAll odabrati sve <li> elemente i svakome postaviti textContent na: “Stavka”?
+document.querySelectorAll("li").forEach(li => li.textContent = "Stavka");
+
+6. Kako pomoću classList.add dodati klasu active na gumb koji ima klasu btn?
+document.querySelector(".btn").classList.add("active");
+
+7. Kako pomoću classList.remove ukloniti klasu list s drugog <li> elementa?
+document.getElementsByTagName("li")[1].classList.remove("list");
+
+8. Kako pristupiti tekstu unutar <h1> i promijeniti ga u “Welcome”? (koristeći innerHTML)
+document.querySelector("h1").innerHTML = "Welcome";
+
+9. Kako dohvatiti tekst prvog <li> elementa koristeći textContent?
+let tekst = document.querySelector("li").textContent;
+
+10. Kako dohvatiti atribut href iz <a> elementa pomoću getAttribute?
+let link = document.querySelector("a").getAttribute("href");*/
--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,34 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+/*11. Kako postaviti novi atribut title="Google link" na <a> element pomoću setAttribute?
+document.querySelector("a").setAttribute("title", "Google link");
+
+12. Kako pomoću querySelector dohvatiti <ul> s ID-em list i dodati mu još jednu stavku pomoću innerHTML?
+document.querySelector("#list").innerHTML += "<li>Nova stavka</li>";
+
+13. Kako pomoću getElementById promijeniti pozadinsku boju cijelog <ul> elementa u svijetloplavu?
+document.getElementById("list").style.backgroundColor = "lightblue";
+
+14. Kako pomoću querySelectorAll odabrati sve <li> elemente i svakome dodati klasu blue-text?
+document.querySelectorAll("li").forEach(li => li.classList.add("blue-text"));
+
+15. Kako dohvatiti checkbox i provjeriti je li označen?
+let checked = document.querySelector("input[type='checkbox']").checked;
+
+16. Kako gumbu .btn promijeniti tekst u “Pressed!” pomoću textContent?
+document.querySelector(".btn").textContent = "Pressed!";
+
+17. Kako pristupiti trećem <li> elementu koristeći getElementsByClassName?
+let third = document.getElementsByClassName("list")[2];
+
+18. Kako pomoću querySelector dohvatiti button.btn i promijeniti mu inline stil: veličinu fonta na 20px?
+document.querySelector("button.btn").style.fontSize = "20px";
+
+19. Kako pomoću getElementsByTagName odabrati prvi <input> i postaviti mu atribut checked na "true"?
+document.getElementsByTagName("input")[0].setAttribute("checked", "true");
+
+20. Kako pomoću querySelectorAll odabrati sve <a> linkove i promijeniti im boju teksta na zelenu?
+document.querySelectorAll("a").forEach(a => a.style.color = "green");*/
